c421abf2b69e53c0168bd782e93f0e3dda0f8eb8
Hugo Henrique <32242824+hugohralm@users.noreply.github.com>
2023-07-31 22:37:30 +0100

    Update create_table_audit_logs.sql

--- create_table_audit_logs.sql
+++ create_table_audit_logs.sql
@@ -1,9 +1,12 @@
-CREATE TABLE audit_logs (
-    id SERIAL PRIMARY KEY,
-    table_name TEXT NOT NULL,
-    row_id TEXT NULL,
-    operation TEXT NOT NULL,
-    changes JSONB,
-    executed_by TEXT NOT NULL,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+CREATE SCHEMA IF NOT EXISTS audit;
+
+CREATE TABLE audit.audit_logs (
+    id serial4 NOT NULL,
+    table_name varchar(150) NOT NULL,
+    row_id varchar(50) NULL,
+    operation varchar(50) NOT NULL,
+    changes jsonb NULL,
+    executed_by varchar(150) NOT NULL,
+    updated_at timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT audit_logs_pkey PRIMARY KEY (id)
 );
