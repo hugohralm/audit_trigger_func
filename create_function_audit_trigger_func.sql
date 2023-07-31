9a011311d2afb40fa1b027c36fafda2294d262cb
Hugo Henrique <32242824+hugohralm@users.noreply.github.com>
2023-07-31 22:31:05 +0100

    Update create_function_audit_trigger_func.sql

--- create_function_audit_trigger_func.sql
+++ create_function_audit_trigger_func.sql
@@ -6,9 +6,19 @@ DECLARE
     v_new_data JSONB;
     v_result JSONB := '{}'::JSONB;
     v_key TEXT;
-    v_row_id TEXT;
+    v_row_id TEXT := NULL;
 BEGIN
-    IF (TG_OP = 'UPDATE') then
+    IF (TG_OP = 'INSERT') THEN
+    	IF hstore(NEW) ? 'id' THEN
+            v_row_id := NEW.id::text;
+        END IF;
+        v_result := to_jsonb(NEW);
+    ELSEIF (TG_OP = 'DELETE') THEN
+        IF hstore(OLD) ? 'id' THEN
+            v_row_id := OLD.id::text;
+        END IF;
+        v_result := to_jsonb(OLD);
+    ELSEIF (TG_OP = 'UPDATE') then
         IF hstore(NEW) ? 'id' THEN
             v_old_data := to_jsonb(OLD);
             v_new_data := to_jsonb(NEW);
@@ -25,25 +35,11 @@ BEGIN
             v_row_id := NULL;
             v_result := to_jsonb(NEW);
         END IF;
-    ELSIF (TG_OP = 'DELETE') THEN
-    	IF hstore(OLD) ? 'id' THEN
-            v_row_id := OLD.id::text;
-        ELSE
-            v_row_id := NULL;
-        END IF;
-        v_result := to_jsonb(OLD);
-    ELSIF (TG_OP = 'INSERT') THEN
-    	IF hstore(NEW) ? 'id' THEN
-            v_row_id := NEW.id::text;
-        ELSE
-            v_row_id := NULL;
-        END IF;
-        v_result := to_jsonb(NEW);
     END IF;
 
     -- Insert v_result into the audit table only if a change was made
     IF v_result <> '{}'::JSONB THEN
-        INSERT INTO audit_logs (table_name, row_id, operation, changes, executed_by) 
+        INSERT INTO audit.audit_logs (table_name, row_id, operation, changes, executed_by) 
         VALUES (TG_TABLE_NAME, v_row_id, TG_OP, v_result, current_user);
     END IF;
 
